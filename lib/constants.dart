ae16526746356a00e5b569baa599ac3810f6fc81
Athola Bahizire <atholabahizire@gmail.com>
2022-09-14 15:51:15 +0200

    uncomment api key corrector

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const String googleAPIKey   = "AIzaSyCP4jiUtiTyml_KULcOek2l2paK22r_HHk";
-// ignore: constant_identifier_names
+
 const String google_api_key = "AIzaSyCP4jiUtiTyml_KULcOek2l2paK22r_HHk";
 const Color primaryColor = Color(0xFF004F92);
 const double defaultPadding = 16.0;
